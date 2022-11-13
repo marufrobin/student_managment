aadc1a97c46db8cc08671f37edf39970c0a47704
Maruf Robin <maruf.robin@northsouth.edu>
2022-11-13 06:56:02 +0600

    Navigator problem solved

---
+++ lib/pages/signup_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign up"),
+      ),
+      body: Container(
+        child: Text("Robin"),
+      ),
+    );
+  }
+}
